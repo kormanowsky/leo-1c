7bcdc52f3a6bde48d307e15491ae1706a1ffd909
Михаил Кормановский <kormanowsky@gmail.com>
2024-05-10 23:25:44 +0300

    remove plural/singular resource name

--- src/leo_1c/odata_resource.py
+++ src/leo_1c/odata_resource.py
@@ -9,31 +9,32 @@ class ODataResource:
             self,
             connection: Connection,
             resource_type: str,
-            resource_name_singular: str,
-            resource_name_plural: str
+            resource_name: str
     ):
         self._conn = connection
         self._resource_type = resource_type
-        self._resource_name_singular = resource_name_singular
-        self._resource_name_plural = resource_name_plural
+        self._resource_name = resource_name
 
     def get_many(self, params: dict = None) -> dict:
         return self._conn.call_odata_handler(
             "GET",
-            f"{self._resource_type}_{self._resource_name_plural}",
+            self._get_odata_handler_name(),
             params
         )
 
     def get_by_guid(self, guid: UUID) -> dict:
         return self._conn.call_odata_handler(
             "GET",
-            f"{self._resource_type}_{self._resource_name_plural}(guid'{guid}')"
+            f"{self._get_odata_handler_name()}(guid'{guid}')"
         )
 
     def create(self, data: dict, params: dict = None) -> dict:
         return self._conn.call_odata_handler(
             "POST",
-            f"{self._resource_type}_{self._resource_name_plural}",
+            self._get_odata_handler_name(),
             params=params,
             body=data
         )
+
+    def _get_odata_handler_name(self) -> str:
+        return f"{self._resource_type}_{self._resource_name}"
